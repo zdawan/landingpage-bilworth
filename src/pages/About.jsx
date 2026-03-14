7d711651fb1b380ad0ab2fee9966b45f5c056eb3
zdawan <kumarjothi704@gmail.com>
2026-03-14 19:35:46 +0530

    profile photo commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 import aboutBanner from "../assets/ab00.jpg";
 import person1 from "../assets/o02.jpg";
-import person2 from "../assets/deepan_Edited.png";
+import person2 from "../assets/001.png";
 
 function About() {
   return (
